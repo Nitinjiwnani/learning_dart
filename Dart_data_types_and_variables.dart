2cce368fa2c3f92a879116076b591aca90a6cb8f
Nitinjiwnani <jiwnani01@gmail.com>
2022-07-10 16:36:23 +0530

    Data types and variables

---
+++ Dart_data_types_and_variables.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Number int
+
+  var count = 545; // It is inferred as integer automatically by  compiler
+  int hexValue = 0xEADEBAEE;
+  //Numbers double
+  double percentage = 93.3;
+
+  //Strings
+
+  var company = "Google";
+
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(hexValue);
+  print(company);
+
+  /*NOTE: All data types are objects by default
+  Therefore, there initial values are null by default
+  print(score);
+  print(name);*/
+}
