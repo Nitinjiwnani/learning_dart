e6f811e2f8ba1b1e2bd93239cd71db3cdbb3b4a1
Nitinjiwnani <jiwnani01@gmail.com>
2022-07-10 16:18:27 +0530

    Hello World

---
+++ HelloWorld.dart
@@ -0,0 +1,9 @@
+//Starting point is main function
+void main() {
+  //This is a comment and a hello world program below
+  print("Hello World");
+
+  print(4 - 2); //It's necessary to add a semicolon after every line
+  print(5 * 5);
+  /* Preferred not to use this as a comment*/
+}
